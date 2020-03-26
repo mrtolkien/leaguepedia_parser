2f0c195bc1a3f41df722cbd3a9993465b29169fa
Gary "Tolki" Mialaret <gary.mialaret@gmail.com>
2020-03-26 13:38:01 +0900

    T E S T D R I V E N D E V E L O P M E N T

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='leaguepedia_parser',
+    version='0.1a',
+    packages=['leaguepedia_parser'],
+    url='https://github.com/mrtolkien/leaguepedia_parser',
+    license='MIT',
+    author='Tolki',
+    author_email='gary.mialaret+pypi@gmail.com',
+    description='A parser for the Leaguepedia website.'
+)
