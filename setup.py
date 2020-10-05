8dd0e366d1d600a7b8799ab55a48a1a82c8d9765
Gary "Tolki" Mialaret <gary.mialaret@gmail.com>
2020-10-05 15:29:17 +0900

    Updated required python version

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setuptools.setup(
     name="leaguepedia_parser",
-    version="1.0",
+    version="1.0.1",
+    python_requires='>=3.8',
     packages=[
         "leaguepedia_parser",
         "leaguepedia_parser/parsers",
